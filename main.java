f4e6663778bf55a7cece4f7ffd7ba5e14aea836c
prashantg-15 <104353965+prashantg-15@users.noreply.github.com>
2023-10-20 12:54:41 +0530

    Update main.java

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ public class main {
         int number = 5; // Change this to the number for which you want to calculate the factorial.
         long factorial = calculateeFactorial(number);
         System.out.println("The factorial of " + number + " is " + factorial);
-    }
+    }}}
 
     public static long calculateFactorial(int n) {
             return n * calculateFactorial(n - 1);
